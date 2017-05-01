646ffc52449a9e387a606b17000343daeac8c842
Bradley Berger <bradley.s.berger@gmail.com>
2017-05-01 17:52:05 -0600

    add tlo directory

--- lib/trans_union.rb
+++ lib/trans_union.rb
@@ -2,4 +2,14 @@ module TransUnion
   def self.smoke_test
     puts "it's smokey alright"
   end
+
+  class << self
+    ## class methods without needing to have the `self.`
+
+    def configure
+      yield self
+      # Allows for TransUnion.configure do { |c| c.user_id = 'blahblah' }
+    end
+
+  end
 end
---
+++ lib/trans_union/tlo/client.rb
@@ -0,0 +1,9 @@
+module TransUnion
+  module TLO
+    class Client
+      def initialize(params)
+        # maybe need this maybe not
+      end
+    end
+  end
+end
