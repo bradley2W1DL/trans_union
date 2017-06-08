f8b093e664f28624d493c61048c959c32006d705
Bradley Berger <bradley.s.berger@gmail.com>
2017-06-07 23:17:19 -0600

    Delete the unused BasicPersonSearch class - also refactor the PersonSearch model to strip whitespace from any value in the search options hash

--- lib/trans_union/tlo/person_search.rb
+++ lib/trans_union/tlo/person_search.rb
@@ -7,46 +7,52 @@ module TransUnion::TLO
     global :convert_request_keys_to, :camelcase
     operations :person_search, :basic_person_search
 
-    def self.person_search(options={})
-      ## returns a Savon::Response object
-      response = super(message: build_request_hash(options))
-      Response.new(response.body)
-    end
+    class << self
 
-    def self.basic_person_search(options={})
-      response = super(message: build_request_hash(options))
-      BasicResponse.new(response.body)
-    end
-
-    def self.build_request_hash(options={})
-      ## hash params are case-sensitive
-      @base_hash = {
-        Username: TransUnion::TLO.username,
-        Password: TransUnion::TLO.password,
-        DPPAPurpose: TransUnion::TLO.dppa_purpose,
-        GLBPurpose: TransUnion::TLO.glb_purpose,
-        PermissibleUseCode: TransUnion::TLO.permissible_use_code
-      }
-
-      { 'genericSearchInput' => @base_hash.merge(parse_options(options)) }
-    end
-
-    private
-
-    def self.parse_options(options)
-      # snake case is mostly ok except for options such as SSN which needs to be all caps
-      ## add any additional normalization into here -- parsing name, etc.
-      #
-      require_upcase = [:ssn]
-      parsed_options = {}
-      options.map do |key, value|
-        if require_upcase.include? key.to_sym
-          parsed_options[key.upcase.to_sym] = value
-        else
-          parsed_options[key.to_sym] = value
-        end
+      def person_search(options={})
+        ## returns a Savon::Response object
+        response = super(message: build_request_hash(options))
+        Response.new(response.body)
       end
-      parsed_options
+
+      def basic_person_search(options={})
+        response = super(message: build_request_hash(options))
+        BasicResponse.new(response.body)
+      end
+
+      def build_request_hash(options={})
+        ## hash params are case-sensitive
+        @base_hash = {
+          Username: TransUnion::TLO.username,
+          Password: TransUnion::TLO.password,
+          DPPAPurpose: TransUnion::TLO.dppa_purpose,
+          GLBPurpose: TransUnion::TLO.glb_purpose,
+          PermissibleUseCode: TransUnion::TLO.permissible_use_code
+        }
+
+        { 'genericSearchInput' => @base_hash.merge(parse_options(options)) }
+      end
+
+      protected
+
+      def parse_options(options)
+        # snake case is mostly ok except for options such as SSN which needs to be all caps
+        ## add any additional normalization into here -- e.g. stripping any whitespace from name
+        #
+        require_upcase = [:ssn]
+        parsed_options = {}
+        options.map do |key, value|
+          if require_upcase.include? key.to_sym
+            parsed_options[key.upcase.to_sym] = value.gsub(/\s/, '')
+          elsif value.is_a? Hash
+            parsed_options[key.to_sym] = self.parse_options(value)
+          else
+            parsed_options[key.to_sym] = value.gsub(/\s/, '')
+          end
+        end
+        parsed_options
+      end
+
     end
   end
 end
