3ff1ed7020f6abfed3962d3e1838eb8345147ebb
Bradley Berger <bradley.s.berger@gmail.com>
2017-07-25 10:11:17 -0600

    WIP write person_search specs

---
+++ spec/tlo/person_search/response_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe TransUnion::TLO::PersonSearch::Response do
+  let(:response) { TransUnion::TLO::PersonSearch::Response }
+
+  let(:response_hash) {
+    {}
+  }
+  describe '.result' do
+    it 'should return the person_search_result' do
+    end
+  end
+end
